278cf5031b5a3574fe7b4b477263f771f1deeac3
Tim Joosten <Tjoosten3@gmail.com>
2017-03-09 16:26:14 +0100

    Add sessions to the controller

--- CodeIgniter-3.1.3/application/controllers/Welcome.php
+++ CodeIgniter-3.1.3/application/controllers/Welcome.php
@@ -3,12 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
+	public $user        = [];
+	public $permissions = [];
+	public $abilities   = [];
+
 	public function __construct()
 	{
 		parent::__construct();
 
-		$this->load->library(['blade']);
+		$this->load->library(['blade', 'session']);
 		$this->load->helper(['url']);
+
+		$this->user        = $this->session->userdata('user');
+		$this->permissions = $this->session->userdata('permissions');
+		$this->abilities   = $this->session->userdata('abilities');
 	}
 
 	public function index()
